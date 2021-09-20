3c1a254712952c89afe7049c28577b34490a4f4d
Chen-Zidi <czdpaige@gmail.com>
2021-09-20 18:30:43 +0200

    client shows response time

--- client/src/main/java/com/grpc/client/GrpcClientService.java
+++ client/src/main/java/com/grpc/client/GrpcClientService.java
@@ -7,6 +7,9 @@ import io.grpc.Channel;
 import net.devh.springboot.autoconfigure.grpc.client.GrpcClient;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.Instant;
+
 @Service
 public class GrpcClientService {
     @GrpcClient("local-grpc-server")
@@ -14,7 +17,11 @@ public class GrpcClientService {
 
     public String sendMessage(String name) {
         HelloGrpc.HelloBlockingStub stub= HelloGrpc.newBlockingStub(serverChannel);
+        Instant inst1 = Instant.now();
         HelloOuterClass.HelloReply response = stub.sayHello(HelloOuterClass.HelloRequest.newBuilder().setName(name).build());
+        Instant inst2 = Instant.now();
+        double diff = Duration.between(inst1, inst2).toMillis();
+        System.out.println("response time:" + diff);
         return response.getMessage();
 
     }
