a4109ff983a0757ae9eefb5d5b4c13874b7cb2bf
Chen-Zidi <czdpaige@gmail.com>
2021-10-02 22:07:20 +0200

    reset spring framework version run request 100 - 1000 times

--- client/src/main/java/com/grpc/client/GrpcClientService.java
+++ client/src/main/java/com/grpc/client/GrpcClientService.java
@@ -28,11 +28,95 @@ public class GrpcClientService {
 
     public String listEmployees() {
         HelloGrpc.HelloBlockingStub stub= HelloGrpc.newBlockingStub(serverChannel);
-        Instant inst1 = Instant.now();
         HelloOuterClass.EmployeeList response = stub.listEmployees(HelloOuterClass.Empty.newBuilder().build());
+
+        Instant inst1 = Instant.now();
+        for(int i = 0; i < 100; i ++){
+            response = stub.listEmployees(HelloOuterClass.Empty.newBuilder().build());
+        }
         Instant inst2 = Instant.now();
         double diff = Duration.between(inst1, inst2).toMillis();
-        System.out.println("response time:" + diff);
+        System.out.println("response time 100:" + diff);
+
+
+        inst1 = Instant.now();
+        for(int i = 0; i < 200; i ++){
+            response = stub.listEmployees(HelloOuterClass.Empty.newBuilder().build());
+        }
+        inst2 = Instant.now();
+        diff = Duration.between(inst1, inst2).toMillis();
+        System.out.println("response time 200:" + diff);
+
+        inst1 = Instant.now();
+        for(int i = 0; i < 300; i ++){
+            response = stub.listEmployees(HelloOuterClass.Empty.newBuilder().build());
+        }
+        inst2 = Instant.now();
+        diff = Duration.between(inst1, inst2).toMillis();
+        System.out.println("response time 300:" + diff);
+
+
+        inst1 = Instant.now();
+        for(int i = 0; i < 400; i ++){
+            response = stub.listEmployees(HelloOuterClass.Empty.newBuilder().build());
+        }
+        inst2 = Instant.now();
+        diff = Duration.between(inst1, inst2).toMillis();
+        System.out.println("response time 400:" + diff);
+
+
+        inst1 = Instant.now();
+        for(int i = 0; i < 500; i ++){
+            response = stub.listEmployees(HelloOuterClass.Empty.newBuilder().build());
+        }
+        inst2 = Instant.now();
+        diff = Duration.between(inst1, inst2).toMillis();
+        System.out.println("response time 500:" + diff);
+
+
+        inst1 = Instant.now();
+        for(int i = 0; i < 600; i ++){
+            response = stub.listEmployees(HelloOuterClass.Empty.newBuilder().build());
+        }
+        inst2 = Instant.now();
+        diff = Duration.between(inst1, inst2).toMillis();
+        System.out.println("response time 600:" + diff);
+
+
+        inst1 = Instant.now();
+        for(int i = 0; i < 700; i ++){
+            response = stub.listEmployees(HelloOuterClass.Empty.newBuilder().build());
+        }
+        inst2 = Instant.now();
+        diff = Duration.between(inst1, inst2).toMillis();
+        System.out.println("response time 700:" + diff);
+
+        inst1 = Instant.now();
+        for(int i = 0; i < 800; i ++){
+            response = stub.listEmployees(HelloOuterClass.Empty.newBuilder().build());
+        }
+        inst2 = Instant.now();
+        diff = Duration.between(inst1, inst2).toMillis();
+        System.out.println("response time 800:" + diff);
+
+
+        inst1 = Instant.now();
+        for(int i = 0; i < 900; i ++){
+            response = stub.listEmployees(HelloOuterClass.Empty.newBuilder().build());
+        }
+        inst2 = Instant.now();
+        diff = Duration.between(inst1, inst2).toMillis();
+        System.out.println("response time 900:" + diff);
+
+
+        inst1 = Instant.now();
+        for(int i = 0; i < 1000; i ++){
+            response = stub.listEmployees(HelloOuterClass.Empty.newBuilder().build());
+        }
+        inst2 = Instant.now();
+        diff = Duration.between(inst1, inst2).toMillis();
+        System.out.println("response time 1000:" + diff);
+
         return response.getEmployeesList().toString();
 
     }
